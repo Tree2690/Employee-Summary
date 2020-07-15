00ae1e30cdc4f50513cd41e287a1330860149142
Jonathan Liang <jonathanliang2690123@gmail.com>
2020-07-15 16:14:03 -0700

    created swutch case

---
+++ app.js
@@ -0,0 +1,41 @@
+const userClass = require("./roles.js")
+
+const employeeRoles =
+[
+    {
+        name: "",
+        prompt: "What type of roles are you?",
+        roles: ['Employee', 'Engineer', 'Intern', 'Manager']
+    }
+]
+
+let askUser = () =>
+{
+    prompt("What type roles are you?")
+        .then(answers =>
+            {
+                switch()
+                {
+                    case 'Employee' :
+                        {
+
+                        }
+                    break;
+                    case 'Engineer' :
+                        {
+
+                        }
+                    break;
+                    case 'Intern' :
+                        {
+
+                        }
+                    break;
+                    case 'Manager' :
+                        {
+
+                        }
+                    break;
+                }
+            })
+}
